d17ba117833af04081f48c140247ea6b04b57ddf
Stefan Härter <sha@rother-oss.com>
2025-07-02 13:53:33 +0200

    Shifted group import and export subs into Autoload module.

---
+++ Kernel/Autoload/GroupImportExport.pm
@@ -0,0 +1,132 @@
+# --
+# OTOBO is a web-based ticketing system for service organisations.
+# --
+# Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
+# Copyright (C) 2019-2025 Rother OSS GmbH, https://otobo.io/
+# --
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# --
+
+## no critic (Modules::RequireExplicitPackage)
+
+use Kernel::System::Group ();    ## no perlimports
+
+package Kernel::System::Group;   ## no critic (Modules::RequireFilenameMatchesPackage)
+
+use strict;
+use warnings;
+use v5.24;
+use utf8;
+
+our @ObjectDependencies = (
+    'Kernel::System::Group',
+    'Kernel::System::Valid',
+);
+
+sub _ExportGroups {
+    my ( $Self, %Param ) = @_;
+
+    my %GroupFilter;
+    if ( IsArrayRefWithData( $Param{Groups} ) ) {
+        %GroupFilter = map { $_ => 1 } $Param{Groups}->@*;
+    }
+
+    my $GroupObject = $Kernel::OM->Get('Kernel::System::Group');
+
+    my %GroupList = $GroupObject->GroupList(
+        Valid => 0,
+    );
+
+    my %ExportData;
+    GROUPID:
+    for my $GroupID ( sort keys %GroupList ) {
+
+        my %GroupData = $GroupObject->GroupGet(
+            ID => $GroupID,
+        );
+
+        if (%GroupFilter) {
+            next GROUPID unless $GroupFilter{ $GroupData{Name} };
+        }
+
+        # translate IDs into names or name-like identifiers
+        my $ValidObject = $Kernel::OM->Get('Kernel::System::Valid');
+
+        ATTRIBUTE:
+        for my $Attribute ( keys %GroupData ) {
+
+            next ATTRIBUTE unless $Attribute =~ /ID/;
+
+            if ( $Attribute eq 'ValidID' ) {
+                my $Valid = $ValidObject->ValidLookup(
+                    ValidID => $GroupData{ValidID},
+                );
+                $GroupData{Valid} = $Valid;
+                delete $GroupData{ValidID};
+            }
+        }
+
+        delete $GroupData{ChangeBy};
+        delete $GroupData{ChangeTime};
+        delete $GroupData{CreateBy};
+        delete $GroupData{CreateTime};
+        delete $GroupData{ID};
+
+        $ExportData{ $GroupData{Name} } = \%GroupData;
+    }
+
+    return \%ExportData;
+}
+
+sub _ImportGroups {
+    my ( $Self, %Param ) = @_;
+
+    my $GroupObject = $Kernel::OM->Get('Kernel::System::Group');
+    my $ValidObject = $Kernel::OM->Get('Kernel::System::Valid');
+    my %GroupList   = $GroupObject->GroupList(
+        Valid => 0,
+    );
+    my %GroupLookup = reverse %GroupList;
+
+    GROUPNAME:
+    for my $GroupName ( keys $Param{Groups}->%* ) {
+        my $GroupData = $Param{Groups}{$GroupName};
+
+        my $GroupID = $GroupLookup{ $GroupData->{Name} };
+
+        # skip if group with same name exists and overwrite is not set
+        next GROUPNAME if ( !$Param{OverwriteExistingEntities} && $GroupID );
+
+        # translate named data back to IDs
+        $GroupData->{ValidID} = $ValidObject->ValidLookup(
+            Valid => $GroupData->{Valid},
+        );
+
+        if ($GroupID) {
+            my $Success = $GroupObject->GroupUpdate(
+                $GroupData->%*,
+                ID     => $GroupID,
+                UserID => $Self->{UserID},
+            );
+            next GROUPNAME unless $Success;
+        }
+        else {
+            my $GroupID = $GroupObject->GroupAdd(
+                $GroupData->%*,
+                UserID => $Self->{UserID},
+            );
+            next GROUPNAME unless $GroupID;
+        }
+    }
+
+    return;
+}
+
+1;
--- Kernel/Modules/AdminGroupImportExport.pm
+++ Kernel/Modules/AdminGroupImportExport.pm
@@ -28,11 +28,9 @@ use List::AllUtils qw(first);
 use Kernel::System::VariableCheck qw(:all);
 
 our @ObjectDependencies = (
-    'Kernel::Config',
     'Kernel::Output::HTML::Layout',
     'Kernel::System::Cache',
     'Kernel::System::Group',
-    'Kernel::System::Log',
     'Kernel::System::Valid',
     'Kernel::System::Web::Request',
     'Kernel::System::YAML',
@@ -52,6 +50,7 @@ sub Run {
 
     # get objects
     my $LayoutObject = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $GroupObject  = $Kernel::OM->Get('Kernel::System::Group');
     my $ParamObject  = $Kernel::OM->Get('Kernel::System::Web::Request');
     my $YAMLObject   = $Kernel::OM->Get('Kernel::System::YAML');
     my $CacheObject  = $Kernel::OM->Get('Kernel::System::Cache');
@@ -136,7 +135,7 @@ sub Run {
                 $GroupsImport{$GroupName} = $ImportData->{Groups}{$GroupName};
             }
 
-            $Self->_ImportGroups(
+            $GroupObject->ImportGroups(
                 Groups                    => \%GroupsImport,
                 OverwriteExistingEntities => $OverwriteExistingEntities,
             );
@@ -175,7 +174,7 @@ sub Run {
 
         if (@Groups) {
 
-            $Data{Groups} = $Self->_ExportGroups(
+            $Data{Groups} = $GroupObject->ExportGroups(
                 Groups => \@Groups,
             );
         }
@@ -327,105 +326,6 @@ sub _GroupShow {
             Name => 'NoDataFound',
             Data => \%Param,
         );
-    }
-
-    return;
-}
-
-sub _ExportGroups {
-    my ( $Self, %Param ) = @_;
-
-    my %GroupFilter;
-    if ( IsArrayRefWithData( $Param{Groups} ) ) {
-        %GroupFilter = map { $_ => 1 } $Param{Groups}->@*;
-    }
-
-    my $GroupObject = $Kernel::OM->Get('Kernel::System::Group');
-
-    my %GroupList = $GroupObject->GroupList(
-        Valid => 0,
-    );
-
-    my %ExportData;
-    GROUPID:
-    for my $GroupID ( sort keys %GroupList ) {
-
-        my %GroupData = $GroupObject->GroupGet(
-            ID => $GroupID,
-        );
-
-        if (%GroupFilter) {
-            next GROUPID unless $GroupFilter{ $GroupData{Name} };
-        }
-
-        # translate IDs into names or name-like identifiers
-        my $ValidObject = $Kernel::OM->Get('Kernel::System::Valid');
-
-        ATTRIBUTE:
-        for my $Attribute ( keys %GroupData ) {
-
-            next ATTRIBUTE unless $Attribute =~ /ID/;
-
-            if ( $Attribute eq 'ValidID' ) {
-                my $Valid = $ValidObject->ValidLookup(
-                    ValidID => $GroupData{ValidID},
-                );
-                $GroupData{Valid} = $Valid;
-                delete $GroupData{ValidID};
-            }
-        }
-
-        delete $GroupData{ChangeBy};
-        delete $GroupData{ChangeTime};
-        delete $GroupData{CreateBy};
-        delete $GroupData{CreateTime};
-        delete $GroupData{ID};
-
-        $ExportData{ $GroupData{Name} } = \%GroupData;
-    }
-
-    return \%ExportData;
-}
-
-sub _ImportGroups {
-    my ( $Self, %Param ) = @_;
-
-    my $GroupObject = $Kernel::OM->Get('Kernel::System::Group');
-    my $ValidObject = $Kernel::OM->Get('Kernel::System::Valid');
-    my %GroupList   = $GroupObject->GroupList(
-        Valid => 0,
-    );
-    my %GroupLookup = reverse %GroupList;
-
-    GROUPNAME:
-    for my $GroupName ( keys $Param{Groups}->%* ) {
-        my $GroupData = $Param{Groups}{$GroupName};
-
-        my $GroupID = $GroupLookup{ $GroupData->{Name} };
-
-        # skip if group with same name exists and overwrite is not set
-        next GROUPNAME if ( !$Param{OverwriteExistingEntities} && $GroupID );
-
-        # translate named data back to IDs
-        $GroupData->{ValidID} = $ValidObject->ValidLookup(
-            Valid => $GroupData->{Valid},
-        );
-
-        if ($GroupID) {
-            my $Success = $GroupObject->GroupUpdate(
-                $GroupData->%*,
-                ID     => $GroupID,
-                UserID => $Self->{UserID},
-            );
-            next GROUPNAME unless $Success;
-        }
-        else {
-            my $GroupID = $GroupObject->GroupAdd(
-                $GroupData->%*,
-                UserID => $Self->{UserID},
-            );
-            next GROUPNAME unless $GroupID;
-        }
     }
 
     return;
