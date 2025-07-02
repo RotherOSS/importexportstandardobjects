bf17892f5f9d07f2dc939ca9ca7b4835df40a849
Stefan Härter <sha@rother-oss.com>
2025-07-02 13:43:58 +0200

    Shifted generic agent import and export subs to autoload module.

--- Kernel/Modules/AdminGenericAgentImportExport.pm
+++ Kernel/Modules/AdminGenericAgentImportExport.pm
@@ -28,19 +28,9 @@ use List::AllUtils qw(first);
 use Kernel::System::VariableCheck qw(:all);
 
 our @ObjectDependencies = (
-    'Kernel::Config',
     'Kernel::Output::HTML::Layout',
     'Kernel::System::Cache',
     'Kernel::System::GenericAgent',
-    'Kernel::System::Lock',
-    'Kernel::System::Log',
-    'Kernel::System::Priority',
-    'Kernel::System::Queue',
-    'Kernel::System::Service',
-    'Kernel::System::SLA',
-    'Kernel::System::State',
-    'Kernel::System::Type',
-    'Kernel::System::User',
     'Kernel::System::Web::Request',
     'Kernel::System::YAML',
 );
@@ -58,10 +48,11 @@ sub Run {
     my ( $Self, %Param ) = @_;
 
     # get objects
-    my $LayoutObject = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
-    my $ParamObject  = $Kernel::OM->Get('Kernel::System::Web::Request');
-    my $YAMLObject   = $Kernel::OM->Get('Kernel::System::YAML');
-    my $CacheObject  = $Kernel::OM->Get('Kernel::System::Cache');
+    my $LayoutObject       = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $GenericAgentObject = $Kernel::OM->Get('Kernel::System::GenericAgent');
+    my $ParamObject        = $Kernel::OM->Get('Kernel::System::Web::Request');
+    my $YAMLObject         = $Kernel::OM->Get('Kernel::System::YAML');
+    my $CacheObject        = $Kernel::OM->Get('Kernel::System::Cache');
 
     $Self->{Subaction} = $ParamObject->GetParam( Param => 'Subaction' ) || '';
 
@@ -143,7 +134,7 @@ sub Run {
                 $GenericAgentsImport{$GenericAgentName} = $ImportData->{GenericAgents}{$GenericAgentName};
             }
 
-            $Self->_ImportGenericAgents(
+            $GenericAgentObject->ImportGenericAgents(
                 GenericAgents             => \%GenericAgentsImport,
                 OverwriteExistingEntities => $OverwriteExistingEntities,
             );
@@ -182,7 +173,7 @@ sub Run {
 
         if (@GenericAgents) {
 
-            $Data{GenericAgents} = $Self->_ExportGenericAgents(
+            $Data{GenericAgents} = $GenericAgentObject->ExportGenericAgents(
                 GenericAgents => \@GenericAgents,
             );
         }
@@ -324,378 +315,6 @@ sub _GenericAgentShow {
             Name => 'NoDataFound',
             Data => \%Param,
         );
-    }
-
-    return;
-}
-
-sub _ExportGenericAgents {
-    my ( $Self, %Param ) = @_;
-
-    my %GenericAgentFilter;
-    if ( IsArrayRefWithData( $Param{GenericAgents} ) ) {
-        %GenericAgentFilter = map { $_ => 1 } $Param{GenericAgents}->@*;
-    }
-
-    my $GenericAgentObject = $Kernel::OM->Get('Kernel::System::GenericAgent');
-    my $LockObject         = $Kernel::OM->Get('Kernel::System::Lock');
-    my $PriorityObject     = $Kernel::OM->Get('Kernel::System::Priority');
-    my $QueueObject        = $Kernel::OM->Get('Kernel::System::Queue');
-    my $ServiceObject      = $Kernel::OM->Get('Kernel::System::Service');
-    my $SLAObject          = $Kernel::OM->Get('Kernel::System::SLA');
-    my $StateObject        = $Kernel::OM->Get('Kernel::System::State');
-    my $TypeObject         = $Kernel::OM->Get('Kernel::System::Type');
-    my $UserObject         = $Kernel::OM->Get('Kernel::System::User');
-
-    my %GenericAgentList = $GenericAgentObject->JobList();
-
-    my %ExportData;
-    GENERICAGENTNAME:
-    for my $GenericAgentName ( sort keys %GenericAgentList ) {
-
-        my %GenericAgentData = $GenericAgentObject->JobGet(
-            Name => $GenericAgentName,
-        );
-
-        if (%GenericAgentFilter) {
-            next GENERICAGENTNAME unless $GenericAgentFilter{ $GenericAgentData{Name} };
-        }
-
-        # translate IDs into names or name-like identifiers
-        ATTRIBUTE:
-        for my $Attribute ( keys %GenericAgentData ) {
-
-            if ( $Attribute eq 'LockIDs' ) {
-                if ( IsArrayRefWithData( $GenericAgentData{LockIDs} ) ) {
-                    my @Locks;
-                    for my $LockID ( $GenericAgentData{LockIDs}->@* ) {
-                        push @Locks, $LockObject->LockLookup(
-                            LockID => $LockID,
-                        );
-                    }
-                    $GenericAgentData{Locks} = \@Locks;
-                    delete $GenericAgentData{LockIDs};
-                }
-            }
-            elsif ( $Attribute eq 'NewLockID' ) {
-                if ( $GenericAgentData{NewLockID} ) {
-                    $GenericAgentData{NewLock} = $LockObject->LockLookup(
-                        LockID => $GenericAgentData{NewLockID},
-                    );
-                    delete $GenericAgentData{NewLockID};
-                }
-            }
-            elsif ( $Attribute eq 'NewOwnerID' ) {
-                if ( $GenericAgentData{NewOwnerID} ) {
-                    $GenericAgentData{NewOwner} = $UserObject->UserLookup(
-                        UserID => $GenericAgentData{NewOwnerID},
-                    );
-                    delete $GenericAgentData{NewOwnerID};
-                }
-            }
-            elsif ( $Attribute eq 'NewPriorityID' ) {
-                if ( $GenericAgentData{NewPriorityID} ) {
-                    $GenericAgentData{NewPriority} = $PriorityObject->PriorityLookup(
-                        PriorityID => $GenericAgentData{NewPriorityID},
-                    );
-                    delete $GenericAgentData{NewPriorityID};
-                }
-            }
-            elsif ( $Attribute eq 'NewQueueID' ) {
-                if ( $GenericAgentData{NewQueueID} ) {
-                    $GenericAgentData{NewQueue} = $QueueObject->QueueLookup(
-                        QueueID => $GenericAgentData{NewQueueID},
-                    );
-                    delete $GenericAgentData{NewQueueID};
-                }
-            }
-            elsif ( $Attribute eq 'NewServiceID' ) {
-                if ( $GenericAgentData{NewServiceID} ) {
-                    $GenericAgentData{NewService} = $ServiceObject->ServiceLookup(
-                        ServiceID => $GenericAgentData{NewServiceID},
-                    );
-                    delete $GenericAgentData{NewServiceID};
-                }
-            }
-            elsif ( $Attribute eq 'NewSLAID' ) {
-                if ( $GenericAgentData{NewSLAID} ) {
-                    $GenericAgentData{NewSLA} = $SLAObject->SLALookup(
-                        SLAID => $GenericAgentData{NewSLAID},
-                    );
-                    delete $GenericAgentData{NewSLAID};
-                }
-            }
-            elsif ( $Attribute eq 'NewStateID' ) {
-                if ( $GenericAgentData{NewStateID} ) {
-                    $GenericAgentData{NewState} = $StateObject->StateLookup(
-                        StateID => $GenericAgentData{NewStateID},
-                    );
-                    delete $GenericAgentData{NewStateID};
-                }
-            }
-            elsif ( $Attribute eq 'NewTypeID' ) {
-                if ( $GenericAgentData{NewTypeID} ) {
-                    $GenericAgentData{NewType} = $TypeObject->TypeLookup(
-                        TypeID => $GenericAgentData{NewTypeID},
-                    );
-                    delete $GenericAgentData{NewTypeID};
-                }
-            }
-            elsif ( $Attribute eq 'OwnerIDs' ) {
-                if ( IsArrayRefWithData( $GenericAgentData{OwnerIDs} ) ) {
-                    my @Owners;
-                    for my $OwnerID ( $GenericAgentData{OwnerIDs}->@* ) {
-                        push @Owners, $UserObject->UserLookup(
-                            UserID => $OwnerID,
-                        );
-                    }
-                    $GenericAgentData{Owners} = \@Owners;
-                    delete $GenericAgentData{OwnerIDs};
-                }
-            }
-            elsif ( $Attribute eq 'PriorityIDs' ) {
-                if ( IsArrayRefWithData( $GenericAgentData{PriorityIDs} ) ) {
-                    my @Priorities;
-                    for my $PriorityID ( $GenericAgentData{PriorityIDs}->@* ) {
-                        push @Priorities, $PriorityObject->PriorityLookup(
-                            PriorityID => $PriorityID,
-                        );
-                    }
-                    $GenericAgentData{Priorities} = \@Priorities;
-                    delete $GenericAgentData{PriorityIDs};
-                }
-            }
-            elsif ( $Attribute eq 'QueueIDs' ) {
-                if ( IsArrayRefWithData( $GenericAgentData{QueueIDs} ) ) {
-                    my @Queues;
-                    for my $QueueID ( $GenericAgentData{QueueIDs}->@* ) {
-                        push @Queues, $QueueObject->QueueLookup(
-                            QueueID => $QueueID,
-                        );
-                    }
-                    $GenericAgentData{Queues} = \@Queues;
-                    delete $GenericAgentData{QueueIDs};
-                }
-            }
-            elsif ( $Attribute eq 'ServiceIDs' ) {
-                if ( IsArrayRefWithData( $GenericAgentData{ServiceIDs} ) ) {
-                    my @Services;
-                    for my $ServiceID ( $GenericAgentData{ServiceIDs}->@* ) {
-                        push @Services, $ServiceObject->ServiceLookup(
-                            ServiceID => $ServiceID,
-                        );
-                    }
-                    $GenericAgentData{Services} = \@Services;
-                    delete $GenericAgentData{ServiceIDs};
-                }
-            }
-            elsif ( $Attribute eq 'SLAIDs' ) {
-                if ( IsArrayRefWithData( $GenericAgentData{SLAIDs} ) ) {
-                    my @SLAs;
-                    for my $SLAID ( $GenericAgentData{SLAIDs}->@* ) {
-                        push @SLAs, $SLAObject->SLALookup(
-                            SLAID => $SLAID,
-                        );
-                    }
-                    $GenericAgentData{SLAs} = \@SLAs;
-                    delete $GenericAgentData{SLAIDs};
-                }
-            }
-            elsif ( $Attribute eq 'StateIDs' ) {
-                if ( IsArrayRefWithData( $GenericAgentData{StateIDs} ) ) {
-                    my @States;
-                    for my $StateID ( $GenericAgentData{StateIDs}->@* ) {
-                        push @States, $StateObject->StateLookup(
-                            StateID => $StateID,
-                        );
-                    }
-                    $GenericAgentData{States} = \@States;
-                    delete $GenericAgentData{StateIDs};
-                }
-            }
-            elsif ( $Attribute eq 'TypeIDs' ) {
-                if ( IsArrayRefWithData( $GenericAgentData{TypeIDs} ) ) {
-                    my @Types;
-                    for my $TypeID ( $GenericAgentData{TypeIDs}->@* ) {
-                        push @Types, $TypeObject->TypeLookup(
-                            TypeID => $TypeID,
-                        );
-                    }
-                    $GenericAgentData{Types} = \@Types;
-                    delete $GenericAgentData{TypeIDs};
-                }
-            }
-        }
-
-        $ExportData{ $GenericAgentData{Name} } = \%GenericAgentData;
-    }
-
-    return \%ExportData;
-}
-
-sub _ImportGenericAgents {
-    my ( $Self, %Param ) = @_;
-
-    my $GenericAgentObject = $Kernel::OM->Get('Kernel::System::GenericAgent');
-    my $LockObject         = $Kernel::OM->Get('Kernel::System::Lock');
-    my $PriorityObject     = $Kernel::OM->Get('Kernel::System::Priority');
-    my $QueueObject        = $Kernel::OM->Get('Kernel::System::Queue');
-    my $ServiceObject      = $Kernel::OM->Get('Kernel::System::Service');
-    my $SLAObject          = $Kernel::OM->Get('Kernel::System::SLA');
-    my $StateObject        = $Kernel::OM->Get('Kernel::System::State');
-    my $TypeObject         = $Kernel::OM->Get('Kernel::System::Type');
-    my $UserObject         = $Kernel::OM->Get('Kernel::System::User');
-    my %GenericAgentList   = $GenericAgentObject->JobList();
-    my %GenericAgentLookup = reverse %GenericAgentList;
-
-    GENERICAGENTNAME:
-    for my $GenericAgentName ( keys $Param{GenericAgents}->%* ) {
-        my $GenericAgentData = $Param{GenericAgents}{$GenericAgentName};
-
-        # TODO check what is exactly stored there and rename attribute accordingly
-        my $GenericAgentID = $GenericAgentLookup{ $GenericAgentData->{Name} };
-
-        # skip if generic agent with same name exists and overwrite is not set
-        next GENERICAGENTNAME if ( !$Param{OverwriteExistingEntities} && $GenericAgentID );
-
-        # translate named data back to IDs
-        if ( IsArrayRefWithData( $GenericAgentData->{Locks} ) ) {
-            my @LockIDs;
-            for my $Lock ( $GenericAgentData->{Locks}->@* ) {
-                push @LockIDs, $LockObject->LockLookup(
-                    Lock => $Lock,
-                );
-            }
-            $GenericAgentData->{LockIDs} = \@LockIDs;
-        }
-        if ( $GenericAgentData->{NewLock} ) {
-            $GenericAgentData->{NewLockID} = $LockObject->LockLookup(
-                Lock => $GenericAgentData->{NewLock},
-            );
-        }
-        if ( $GenericAgentData->{NewOwner} ) {
-            $GenericAgentData->{NewOwnerID} = $UserObject->UserLookup(
-                UserLogin => $GenericAgentData->{NewOwner},
-            );
-        }
-        if ( $GenericAgentData->{NewPriority} ) {
-            $GenericAgentData->{NewPriorityID} = $PriorityObject->PriorityLookup(
-                Priority => $GenericAgentData->{NewPriority},
-            );
-        }
-        if ( $GenericAgentData->{NewQueue} ) {
-            $GenericAgentData->{NewQueueID} = $QueueObject->QueueLookup(
-                Queue => $GenericAgentData->{NewQueue},
-            );
-        }
-        if ( $GenericAgentData->{NewService} ) {
-            $GenericAgentData->{NewServiceID} = $ServiceObject->ServiceLookup(
-                Name => $GenericAgentData->{NewService},
-            );
-        }
-        if ( $GenericAgentData->{NewSLA} ) {
-            $GenericAgentData->{NewSLAID} = $SLAObject->SLALookup(
-                Name => $GenericAgentData->{NewSLA},
-            );
-        }
-        if ( $GenericAgentData->{NewState} ) {
-            $GenericAgentData->{NewStateID} = $StateObject->StateLookup(
-                State => $GenericAgentData->{NewState},
-            );
-        }
-        if ( $GenericAgentData->{NewType} ) {
-            $GenericAgentData->{NewTypeID} = $TypeObject->TypeLookup(
-                Type => $GenericAgentData->{NewType},
-            );
-        }
-        if ( IsArrayRefWithData( $GenericAgentData->{Owners} ) ) {
-            my @OwnerIDs;
-            for my $Owner ( $GenericAgentData->{Owners}->@* ) {
-                push @OwnerIDs, $UserObject->UserLookup(
-                    UserLogin => $Owner,
-                );
-            }
-            $GenericAgentData->{OwnerIDs} = \@OwnerIDs;
-        }
-        if ( IsArrayRefWithData( $GenericAgentData->{Priorities} ) ) {
-            my @PriorityIDs;
-            for my $Priority ( $GenericAgentData->{Priorities}->@* ) {
-                push @PriorityIDs, $PriorityObject->PriorityLookup(
-                    Priority => $Priority,
-                );
-            }
-            $GenericAgentData->{PriorityIDs} = \@PriorityIDs;
-        }
-        if ( IsArrayRefWithData( $GenericAgentData->{Queues} ) ) {
-            my @QueueIDs;
-            for my $Queue ( $GenericAgentData->{Queues}->@* ) {
-                push @QueueIDs, $QueueObject->QueueLookup(
-                    Queue => $Queue,
-                );
-            }
-            $GenericAgentData->{QueueIDs} = \@QueueIDs;
-        }
-        if ( IsArrayRefWithData( $GenericAgentData->{Services} ) ) {
-            my @ServiceIDs;
-            for my $Service ( $GenericAgentData->{Services}->@* ) {
-                push @ServiceIDs, $ServiceObject->ServiceLookup(
-                    Name => $Service,
-                );
-            }
-            $GenericAgentData->{ServiceIDs} = \@ServiceIDs;
-        }
-        if ( IsArrayRefWithData( $GenericAgentData->{SLAs} ) ) {
-            my @SLAIDs;
-            for my $SLA ( $GenericAgentData->{SLAs}->@* ) {
-                push @SLAIDs, $SLAObject->SLALookup(
-                    Name => $SLA,
-                );
-            }
-            $GenericAgentData->{SLAIDs} = \@SLAIDs;
-        }
-        if ( IsArrayRefWithData( $GenericAgentData->{States} ) ) {
-            my @StateIDs;
-            for my $State ( $GenericAgentData->{States}->@* ) {
-                push @StateIDs, $StateObject->StateLookup(
-                    State => $State,
-                );
-            }
-            $GenericAgentData->{StateIDs} = \@StateIDs;
-        }
-        if ( IsArrayRefWithData( $GenericAgentData->{Types} ) ) {
-            my @TypeIDs;
-            for my $Type ( $GenericAgentData->{Types}->@* ) {
-                push @TypeIDs, $TypeObject->TypeLookup(
-                    Type => $Type,
-                );
-            }
-            $GenericAgentData->{TypeIDs} = \@TypeIDs;
-        }
-
-        if ($GenericAgentID) {
-
-            # remove/clean up old profile stuff
-            $GenericAgentObject->JobDelete(
-                Name   => $GenericAgentID,
-                UserID => $Self->{UserID},
-            );
-
-            # insert new profile params
-            my $Success = $GenericAgentObject->JobAdd(
-                Name   => $GenericAgentID,
-                Data   => $GenericAgentData,
-                UserID => $Self->{UserID},
-            );
-            next GENERICAGENTNAME unless $Success;
-        }
-        else {
-            my $GenericAgentID = $GenericAgentObject->JobAdd(
-                Name   => $GenericAgentData->{Name},
-                Data   => $GenericAgentData,
-                UserID => $Self->{UserID},
-            );
-            next GENERICAGENTNAME unless $GenericAgentID;
-        }
     }
 
     return;
